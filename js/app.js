d3c3adc1492f0d5684ed3f6fc0faa3e41393eacd
lotusgraham <gwhitley99@gmail.com>
2016-04-11 15:26:40 -0400

    playing sound and shooting hadouken

--- js/app.js
+++ js/app.js
@@ -8,18 +8,27 @@ $(document).ready(function(){
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
+		playHadouken();
  		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
- 		$('.hadouken').show();
-	 // play hadouken sound
+ 		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left','520px');
+			}
+		);
 	})
 	.mouseup(function() {
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
-  	  // ryu goes back to his ready position
-
-  	  // abc1234567890
-
 	});
 
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
